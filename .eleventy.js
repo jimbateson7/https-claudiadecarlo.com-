574449f23c45b95bbec6acb3ee236748b5ace0a0
James Bateson <jim.bateson@outlook.com>
2025-06-07 17:00:58 +0100

    feat: placeholder favicon

--- .eleventy.js
+++ .eleventy.js
@@ -32,7 +32,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/static/img");
 
   // Copy favicon to root of /_site
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/static/favicons");
 
   // Copy robots.txt
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
