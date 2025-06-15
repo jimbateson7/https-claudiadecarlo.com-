6d1a89b90c9111c78cc4375b1b3a0285bcf0e839
James Bateson <jim.bateson@outlook.com>
2025-06-15 15:05:48 +0100

    fix: bit of a hacky fix for splide autoheight on mobile

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -4,6 +4,6 @@ document.addEventListener( 'DOMContentLoaded', function() {
     var elms = document.getElementsByClassName( 'splide' );
 
     for ( var i = 0; i < elms.length; i++ ) {
-        new Splide( elms[ i ] ).mount();
+        new Splide( elms[ i ] ).mount();        
     }
 });
