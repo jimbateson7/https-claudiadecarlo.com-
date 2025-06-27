724a3cb0595edd280f2cdc277c789e11bf8db16c
James Bateson <jim.bateson@outlook.com>
2025-06-27 16:50:25 +0100

    feat: trying out some animations

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,4 +1,7 @@
 import Splide from '@splidejs/splide';
+import { gsap } from 'gsap';
+
+// Instantiate Splide for all elements with the class 'splide'
 
 document.addEventListener( 'DOMContentLoaded', function() {
     var elms = document.getElementsByClassName( 'splide' );
@@ -7,3 +10,25 @@ document.addEventListener( 'DOMContentLoaded', function() {
         new Splide( elms[ i ] ).mount();        
     }
 });
+
+// GSAP Animations
+
+// Home hero avatar decoration
+if (document.querySelector('.js-avatar-decoration')) {
+    gsap.to('.js-avatar-decoration', {
+        duration: 1,
+        opacity: 0.25, 
+        delay: 0.25,
+        ease: 'sine.out',
+        y: -20
+    });
+
+    document.querySelectorAll('.js-avatar-decoration__line').forEach(function() {
+        gsap.to('.js-avatar-decoration__line', {
+        duration: 0.75, 
+        opacity: 1,  
+        stagger: 0.1,
+        ease: 'back.in'
+        });
+    });
+}
