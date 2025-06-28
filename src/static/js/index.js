00cd7ab03aa3097d93102c96a99b344de4524666
James Bateson <jim.bateson@outlook.com>
2025-06-28 10:32:25 +0100

    style: little timing tweak for the homepage hero animation and added an initial effort of the footer one

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,5 +1,9 @@
 import Splide from '@splidejs/splide';
 import { gsap } from 'gsap';
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger);
 
 // Instantiate Splide for all elements with the class 'splide'
 
@@ -18,17 +22,52 @@ if (document.querySelector('.js-avatar-decoration')) {
     gsap.to('.js-avatar-decoration', {
         duration: 1,
         opacity: 0.25, 
-        delay: 0.25,
+        delay: 0.15,
         ease: 'sine.out',
         y: -20
     });
 
     document.querySelectorAll('.js-avatar-decoration__line').forEach(function() {
         gsap.to('.js-avatar-decoration__line', {
-        duration: 0.75, 
-        opacity: 1,  
-        stagger: 0.1,
-        ease: 'back.in'
+            duration: 0.75, 
+            opacity: 1,  
+            stagger: 0.1,
+            ease: 'back.in'
         });
     });
 }
+
+// Animate the footer decoration
+const footerPath = document.querySelector('.js-footer-decoration');
+
+if (footerPath) {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.footer',
+        start: 'top 80%',
+        toggleActions: 'play none none none',
+        once: true
+      }
+    });
+  
+    // Step 1: Initial state
+    gsap.set(footerPath, {
+      drawSVG: '0%',
+      y: 200,
+      fill: 'transparent',
+      transformOrigin: 'bottom'
+    });
+  
+    tl.to(footerPath, {
+      drawSVG: '100%',
+      y: 0,
+      duration: 3.5,
+      ease: 'power2.out'
+    });
+
+    tl.to(footerPath, {
+      fill: 'var(--color-brand-yellow)',
+      duration: 1,
+      ease: 'power2.out'
+    }, '+=0.2');
+}  
